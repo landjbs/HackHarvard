b947d85fa51e5dd1f24714c0a8f25d70a3689a17
Landon Smith <landjbs@gmail.com>
2019-10-18 20:46:35 -0400

    Summarizer_Model init

---
+++ models/summarizer.py
@@ -0,0 +1,12 @@
+
+
+
+
+class Summarizer_Model():
+    """
+    Model to summarize text by modifying BERT vec using LSTM or Dense Net
+    """
+    def __init__(self):
+        self.BERT_DIM = 1024
+
+    def
