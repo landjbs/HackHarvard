610e2aace8e0902eb481d2108514c513485f043e
Landon Smith <landjbs@gmail.com>
2019-10-19 15:08:44 -0400

    gitignore updates

--- cocoReader.py
+++ cocoReader.py
@@ -3,8 +3,8 @@ Reads image/caption pairs from coco dataset, encoding captions and images
 into sampleTensor.
 """
 
-from pycocotools.coco import COCO
-import numpy as np
-import skimage.io as io
-import matplotlib.pyplot as plt
-import pylab
+# from pycocotools.coco import COCO
+# import numpy as np
+# import skimage.io as io
+# import matplotlib.pyplot as plt
+# import pylab
