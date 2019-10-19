2565037d95e4542820a3961de079d2e8a2a391c4
Landon Smith <landjbs@gmail.com>
2019-10-18 21:28:31 -0400

    topModel prog

---
+++ models/generator.py
@@ -0,0 +1,8 @@
+
+
+class Generator(object):
+    """
+    Generator maps from latent space to image space. Used in both discriminator
+    and describer.
+    """
+    def __init__(self, )
---
+++ topModel.py
@@ -0,0 +1,48 @@
+from utils import assert_type
+
+class Image_Generator():
+    """
+    Top-level model comprising summarizer, generator, discriminator, and
+    describer.
+    """
+
+    def __init__(self, rowNum, colNum):
+        # assertions
+        assert_type('rowNum', rowNum, int)
+        assert_type('colNum', colNum, int)
+        ## text specs ##
+        self.embeddeingDim  = 1024
+        ## image specs ##
+        self.rowNum         = rowNum
+        self.colNum         = colNum
+        self.CHANNEL_NUM    = 3
+        ## object info ##
+        self.curIter        = 0
+        self.summarizer     = None
+        self.generator      = None
+        self.discriminator  = None
+        self.describer      = None
+        self.init           = False
+        ## training specs ##
+        # default first-layer filter depth of discriminator
+        DIS_DEPTH               =   64
+        self.DIS_DEPTH          =   DIS_DEPTH
+        self.GEN_DEPTH          =   DIS_DEPTH * 4
+        # default dropout; should prevent memorization
+        self.DROPOUT            =   0.2
+        # default kernel size
+        self.KERNEL_SIZE        =   5
+        # default convolution stride length
+        self.STRIDE             =   2
+        # default alpha of LeakyReLU activation in discriminator
+        self.LEAKY_ALPHA        =   0.2
+        # dimensions of generator latent space
+        self.LATENT_DIMS        =   100
+        # default momentum for adjusting mean and var in generator batch norm
+        self.NORM_MOMENTUM      =   0.9
+
+    def __str__(self):
+        return (f'<Image_Generator Model | INIT={self.init} '
+                f'| ITER={self.curIter}>')
+
+    def build_summarizer(self):
