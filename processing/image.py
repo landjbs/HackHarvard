bbfc10fab3de3f9c2889c1a85cec12295888ad3b
Landon Smith <landjbs@gmail.com>
2019-10-19 14:31:54 -0400

    filter_image prog

--- processing/image.py
+++ processing/image.py
@@ -3,23 +3,17 @@ Utils for cleaning and processing all images encountered by models.
 """
 
 
-def filter_image(imArray, outDim=256, ):
+def filter_image(imArray, outDim=256, upperBound=1024):
     """
     Filters imArray to outDim, filtering those with dimensions
-    outside of upper- and higherBound.
+    outside of outDim and upperBound. Raises error if
     """
-    if 256 <= imArray.shape[0] <= 1024:
-        if 258 <= imArray.shape[1] <= 1024:
-            # print("cropping")
-            hOffset = int((imArray.shape[0] - 256)/2)
-            wOffset = int((imArray.shape[1] - 258)/2)
-            imArray = imArray[hOffset:hOffset + 256,
-                            wOffset:wOffset + 258,:]
-        else:
-            scrapeMetrics.add(True)
-            urlQueue.task_done()
-            continue
+    imShape = imArray.shape
+    if ((outDim <= imShape[0] <= upperBound)
+        and ((outDim + 2) <= imShape[1] <= upperBound)):
+            hOffset = int((imShape[0] - outDim)/2)
+            wOffset = int((imShape[1] - outDim)/2)
+            imArray = imArray[hOffset:hOffset + outDim,
+                            wOffset:wOffset + outDim, :]
     else:
-        scrapeMetrics.add(True)
-        urlQueue.task_done()
-        continue
+        raise InputError(f'Image has invalid dims {imShape}.')
