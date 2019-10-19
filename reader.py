c9595d2e20c8e01585cbe16435e4b142a8cba825
Harrison-Oatman <hroatman@college.harvard.edu>
2019-10-18 21:45:56 -0400

    Functional image fetcher

--- reader.py
+++ reader.py
@@ -36,48 +36,47 @@ def read_dataset(filePath, batchSize):
             assert (len(lineSplit)==2), ('line expected length 2, but found '
                                         f'length {len(lineSplit)}')
             lineCap = lineSplit.pop(0)
-            cleanedURL = re.sub(secureMatcher, "http", lineSplit[0])
-            imgFetch = requests.get(cleanedURL, auth=('user', 'pass'))
+            cleanedURL = re.sub(secureMatcher, "http", lineSplit[0].strip("\n"))
+            imgFetch = requests.get(cleanedURL, headers={'User-Agent': 'Custom'})
             print(cleanedURL)
             print(imgFetch)
-            try:
-                img = Image.open(BytesIO(imgFetch.content))
-            except IOError:
-                continue
-            print(img)
+            # try:
+            #     img = Image.open(BytesIO(imgFetch.content))
+            # except IOError:
+            #     continue
+            # print(img)
 
 
 read_dataset('data/inData/captionsTrain.tsv', 1)
 
-
 ## LANDONS CODE ##
-import re
-class ParseError(Exception):
-    """ Exception for errors while parsing a link """
-    pass
-
-
-# matcher for url denoted by https:// or http://
-urlString = r'https://\S+|http://\S+'
-urlMatcher = re.compile(urlString)
-
-
-def parsable(url):
-    """ Returns true if url follows urlMatcher pattern """
-    return True if urlMatcher.fullmatch(url) else False
-
-
-def fix_url(url, rootURL):
-    """ Add proper headings URLs for crawler analysis """
-    urlString = str(url)
-    if not parsable(urlString):
-        if urlString.startswith('http'):
-            pass
-        elif urlString.startswith("www"):
-            urlString = "https://" + urlString
-        elif urlString.startswith('/'):
-            urlString = rootURL + urlString
-            print(urlString, rootURL)
-        else:
-            urlString = "http://www." + urlString
-    return urlString
+# class ParseError(Exception):
+#     """ Exception for errors while parsing a link """
+#     pass
+#
+#
+# # matcher for url denoted by https:// or http://
+# urlString = r'https://\S+|http://\S+'
+# urlMatcher = re.compile(urlString)
+#
+#
+# def parsable(url):
+#     """ Returns true if url follows urlMatcher pattern """
+#     return True if urlMatcher.fullmatch(url) else False
+#
+#
+# def fix_url(url, rootURL):
+#     """ Add proper headings URLs for crawler analysis """
+#     urlString = str(url)
+#     if not parsable(urlString):
+#         if urlString.startswith('http'):
+#             pass
+#         elif urlString.startswith("www"):
+#             urlString = "https://" + urlString
+#         elif urlString.startswith('/'):
+#             urlString = rootURL + urlString
+#             print(urlString, rootURL)
+#         else:
+#             urlString = "http://www." + urlString
+#     return urlString
+#         embeddingDim dimensions to third-r
