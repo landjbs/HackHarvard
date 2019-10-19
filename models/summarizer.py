66e78279fcd3dfd72419fdc09358692deb7ac268
Landon Smith <landjbs@gmail.com>
2019-10-18 21:05:25 -0400

    build_summarizer struct prog

--- models/summarizer.py
+++ models/summarizer.py
@@ -5,10 +5,11 @@ class Summarizer_Model():
     """
     Model to summarize text by modifying BERT vec using LSTM or Dense Net
     """
-    def __init__(self):
+    def __init__(self, maxLen):
         # length of bert vectors
         self.BERT_DIM               = 1024
         self.LATENT_DIM             = 1024
+        self.MAX_CAPTION_LEN        = maxLen
         # architecture of summarizer model
         self.summarizerStructure    = None
         # compiled summarizer model
@@ -16,8 +17,17 @@ class Summarizer_Model():
 
     def build_summarizer(self):
         """ Builds architecture of summarizer """
-        inputs = Input(shape=(self.BERT_DIM, ), name='encodings')
-        lstm_in = LSTM(units=self.LATENT_DIM, activation='relu', )
+        # matix of word encodings
+        word_inputs = Input(shape=(self.BERT_DIM, self.MAX_CAPTION_LEN),
+                            name='word_encodings')
+        # vector of cls
+        cls_input = Input(shape=(self.BERT_DIM, ), name='cls_input')
+        # define structure of bidirectional lstm
+        lstm_in = Bidirectional(LSTM(units=self.LATENT_DIM, activation='relu',
+                                    return_state=return_state))
+        # get outputs of lstm run over word_inputs with cls initial state
+        lstm_out = Bidirectional(lstm_in(word_inputs, intial_state=cls_input))
+
 
 inputs = keras.layers.Input(shape=[404, 768], name='encodings')
     # gru_out = keras.layers.Bidirectional(keras.layers.GRU(units=768, return_sequences=True))(inputs)
